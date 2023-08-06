0c9074442e5ac5a105d1bac5636cee8b0dead091
asfaquralam1 <ahsansagor449@gmail.com>
2023-08-06 11:47:43 +0600

    update branch

--- app/Http/Controllers/SizeController.php
+++ app/Http/Controllers/SizeController.php
@@ -7,79 +7,57 @@ use Illuminate\Http\Request;
 
 class SizeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $size['data'] = Size::all();
+        return view('admin.size',$size);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function create(Request $request)
     {
-        //
-    }
+        $request->validate([
+            'size'=>'required|unique:sizes',
+        ]);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+        $modal = new Size();
+        $modal->size = $request->post('size');
+        $modal->save();
+        $request->session()->flash('message', 'Size Inserted');
+        return redirect('admin/size');
+    }
     public function store(Request $request)
     {
         //
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Size  $size
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Size $size)
+    public function show()
     {
-        //
+        return view('admin.manage_size');
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Size  $size
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Size $size)
+    public function edit($id)
     {
-        //
+        $modal['size'] = Size::find($id);
+        return view('admin.edit_coupon',$modal);
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Size  $size
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Size $size)
+    public function update(Request $request,$id)
     {
-        //
+        $modal = Size::find($id);
+        $modal->size = $request->post('size');
+        $modal->update();
+        $request->session()->flash('message', 'Size Updated');
+        return redirect('admin/size');
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Size  $size
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Size $size)
+    public function destroy($id,Request $request)
     {
-        //
+        $size = Size::find($id);
+        $size->delete();
+        $request->session()->flash('message', 'Coupon Deleted');
+        return redirect('admin/size');
+    }
+    public function status(Request $request,$status,$id)
+    {
+        $size = Size::find($id);
+        $size->status= $status;
+        $size->save();
+        $request->session()->flash('message', 'size Status updated');
+        return redirect('admin/size');
     }
 }
---
+++ resources/views/admin/manage_size.blade.php
@@ -0,0 +1,38 @@
+@extends('admin.layout')
+@section('page_title','Manage Size')
+@section('size_select','active')
+@section('container')
+<h1 class="mb-10">Manage Size</h1>
+
+<a href="{{ route('admin/size')}}">
+    <button type="button" class="btn btn-success">Back</button>
+</a>
+
+<div class="row m-t-30">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-title">
+                    <h3 class="text-center title-2">Add Size</h3>
+                </div>
+                <hr>
+                <form action="{{ route('size.add') }}" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label for="size" class="control-label mb-1">Size Title</label>
+                        <input id="size" name="size" type="text" class="form-control" aria-required="true" aria-invalid="false" required>
+                        @error('size')
+                        <div class="text-center text-danger">{{ $message}}</div>
+                        @enderror
+                    </div>
+                    <div>
+                        <button id="payment-button" type="submit" class="btn btn-lg btn-info btn-block">
+                            submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
