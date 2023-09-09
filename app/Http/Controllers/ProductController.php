b9665be99016b0d9211277ec6e0e17ed9cf90df7
asfaquralam1 <ahsansagor449@gmail.com>
2023-09-09 23:02:40 +0600

    product updated

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -52,7 +52,7 @@ class ProductController extends Controller
         $color['color'] = Color::all();
         $coupon['coupon'] = Coupon::all();
         $size['size'] = Size::all();
-        return view('admin.manage_product',$category,$size)->with($color)->with($coupon);
+        return view('admin.manage_product',compact('category','size','color','coupon'));
     }
 
     public function edit(product $product)
