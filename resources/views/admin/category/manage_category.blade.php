c007aaee49a2608125730c6ebf29961b44568d6e
asfaquralam1 <ahsansagor449@gmail.com>
2024-07-09 12:02:36 +0600

    laravel admin product ui update

--- resources/views/admin/category/manage_category.blade.php
+++ resources/views/admin/category/manage_category.blade.php
@@ -3,27 +3,40 @@
 @section('category_select', 'active')
 @section('container')
     <h1>Category</h1>
-    <div class="row">
-            @foreach ($categories as $category)
-                <p>Category Name : {{ $category->name }}</p>
-                <p>Category Slug : {{ $category->slug }}</p>
-                @if ($category->status == 1)
-                    <a href="{{ url('admin/category/status/0') }}/{{ $category->id }}">
-                        <button type="submit" class="btn btn-success"
-                            style="padding: 5px 18px 5px 18px !important">Active</button>
-                    </a>
-                @elseif ($category->status == 0)
-                    <a href="{{ url('admin/category/status/1') }}/{{ $category->id }}">
-                        <button type="submit" class="btn btn-secondary"
-                            style="padding: 5px 18px 5px 18px !important">Deactive</button>
-                    </a>
-                @endif
-                <a href="{{ route('admin/edit-category', $category->id) }}"><button>Edit</button></a>
-                <form action="{{ route('admin/destory-category', $category->id) }}" method="post">
-                    @method('DELETE')
-                    @csrf
-                    <button>Delete</button>
-                </form>
-            @endforeach
-        </div>
+    <table class="table">
+        <tr>
+            <th>Category Name</th>
+            <th>Category Slug</th>
+            <th colspan="3">action</th>
+        </tr>
+        @foreach ($categories as $category)
+            <tr>
+                <td>{{ $category->name }}</td>
+                <td>{{ $category->slug }}</td>
+                <td>
+                    @if ($category->status == 1)
+                        <form action="{{ url('admin/category/status/0') }}/{{ $category->id }}" method="post">
+                            @csrf
+                            <button type="submit" class="btn btn-success status-btn">Active</button>
+                        </form>
+                    @elseif ($category->status == 0)
+                        <form action="{{ url('admin/category/status/1') }}/{{ $category->id }}" method="post">
+                            @csrf
+                            <button type="submit" class="btn btn-secondary status-btn"
+                                style="padding: 5px 18px 5px 18px !important">Deactive</button>
+                        </form>
+                    @endif
+                </td>
+                <td><a href="{{ route('admin/edit-category', $category->id) }}"><button class="edit-btn">Edit</button></a>
+                </td>
+                <td>
+                    <form action="{{ route('admin/destory-category', $category->id) }}" method="post">
+                        @method('DELETE')
+                        @csrf
+                        <button class="delete-btn">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
--- resources/views/admin/product/manage_product.blade.php
+++ resources/views/admin/product/manage_product.blade.php
@@ -2,34 +2,36 @@
 @section('page_title', 'Product')
 @section('product_select', 'active')
 @section('container')
-<h1>Product</h1>
-<button class="add-btn"><a href="{{ route('admin/manage-product') }}">Add Product</a></button>
+    <h1>Product</h1>
+    <button class="add-btn"><a href="{{ route('admin/manage-product') }}">Add Product</a></button>
 
-<table class="table">
-    <tr>
-        <th>Name</th>
-        <th>Category</th>
-        <th>Details</th>
-        <th>Price</th>
-        <th>Discount Price</th>
-        <th>image</th>
-        <th colspan="3">action</th>
-    </tr>
-    <tr>
-    @foreach ($products as $product)
-    <td>{{ $product->name }}</td>
-    <td>{{ $product->category }}</td>
-    <td>{{ $product->details }}</td>
-    <td>{{ $product->price }}</td>
-    <td>{{ $product->discount_price }}</td>
-    <td><img src="/product/{{ $product->image }}" alt="{{ $product->name }}" class="product_image"></td>
-    <td><a href="{{ route('admin/edit-product', $product->id) }}"><button class="edit-btn">Edit</button></a></td>
-    <td><form action="{{ route('admin/destory-product', $product->id) }}" method="post">
-        @method('DELETE')
-        @csrf
-        <button class="delete-btn">Delete</button>
-    </form></td>
-    @endforeach
-    </tr>
-</table>
+    <table class="table">
+        <tr>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Details</th>
+            <th>Price</th>
+            <th>Discount Price</th>
+            <th>image</th>
+            <th colspan="3">action</th>
+        </tr>
+        @foreach ($products as $product)
+            <tr>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->category }}</td>
+                <td>{{ $product->details }}</td>
+                <td>{{ $product->price }}</td>
+                <td>{{ $product->discount_price }}</td>
+                <td><img src="/product/{{ $product->image }}" alt="{{ $product->name }}" class="product_image"></td>
+                <td><a href="{{ route('admin/edit-product', $product->id) }}"><button class="edit-btn">Edit</button></a></td>
+                <td>
+                    <form action="{{ route('admin/destory-product', $product->id) }}" method="post">
+                        @method('DELETE')
+                        @csrf
+                        <button class="delete-btn">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
