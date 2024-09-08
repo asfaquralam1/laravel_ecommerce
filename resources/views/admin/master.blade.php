5ce3bcdacdd6857b49f096e71420b4890d96bfef
unknown <ahsansagor449@gmail.com>
2024-09-09 00:56:45 +0600

    datatable update

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -23,6 +23,9 @@
     @section('container')
     @show
     <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="{{ asset('js/datatables.min.js') }}"></script>
+	<script src="{{ asset('js/data-table.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
--- resources/views/admin/product/manage_product.blade.php
+++ resources/views/admin/product/manage_product.blade.php
@@ -2,16 +2,59 @@
 @section('page_title', 'Product')
 @section('product_select', 'active')
 @section('container')
-    <div class="row">
-        <div class="col-2">
-            @include('admin.sidebar')
+<div class="row">
+    <div class="col-2">
+        @include('admin.sidebar')
+    </div>
+    <div class="col-10">
+        <div style="display: flex;justify-content:space-between">
+            <h1 style="text-align: center">Product</h1>
+            <button class="add-btn"><a href="{{ route('admin/manage-product') }}">Add Product</a></button>
         </div>
-        <div class="col-10">
-            <div style="display: flex;justify-content:space-between">
-                <h1 style="text-align: center">Product</h1>
-                <button class="add-btn"><a href="{{ route('admin/manage-product') }}">Add Product</a></button>
-            </div>
-            <table class="table">
+        <table id="example1" class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Category</th>
+                    <th>Details</th>
+                    <th>Price</th>
+                    <th>Discount Price</th>
+                    <th>Quantity</th>
+                    <th>image</th>
+                    <th>action</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach ($products as $product)
+                    <tr>
+                        <td>{{ $product->name }}</td>
+                        <td>{{ $product->category }}</td>
+                        <td>{{ $product->details }}</td>
+                        <td>{{ $product->price }}</td>
+                        <td>{{ $product->discount_price }}</td>
+                        <td>{{ $product->quantity }}</td>
+                        <td></td>
+                        <td></td>
+                        <!-- <td><img src="/product/{{ $product->image }}" alt="{{ $product->name }}" class="product_image"></td>
+                        <td>
+                            <button class="edit-btn">
+                                <a href="{{ route('admin/edit-product', $product->id) }}"><i class="fas fa-edit"></i>
+                                </a></button>
+                        </td>
+                        <td>
+                            <button class="delete-btn">
+                                <form action="{{ route('admin/destory-product', $product->id) }}" method="post">
+                                    @method('DELETE')
+                                    @csrf
+                                    <i class="fas fa-trash"></i>
+                                </form>
+                            </button>
+                        </td> -->
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <!-- <table class="table">
                 <tr>
                     <th>Name</th>
                     <th>Category</th>
@@ -47,7 +90,7 @@
                         </td>
                     </tr>
                 @endforeach
-            </table>
-        </div>
+            </table> -->
     </div>
+</div>
 @endsection
