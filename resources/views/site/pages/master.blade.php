bb61ac8087c0c6b494db9695d4af6eff3c8cdbc3
unknown <ahsansagor449@gmail.com>
2024-10-18 23:56:25 +0600

    realted image&product update

--- resources/views/site/pages/master.blade.php
+++ resources/views/site/pages/master.blade.php
@@ -9,8 +9,14 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <!-- Fontawsome CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    
+    <!-- OwlCarousel -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
 
     <title>Laravel</title>
     <link rel="icon" href="{{ asset('image/favicon.png') }}" type="image/png">
@@ -28,6 +34,25 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $(".owl-carousel").owlCarousel({
+                items: 4, // Number of items to display
+                loop: true,
+                // nav: true,
+                // dots: true,
+                autoplay: true,
+                utoplayTimeout: 1000,
+                autoplayHoverPause: true,
+                navText: [
+                    '<i class="fas fa-chevron-left"></i>',
+                    '<i class="fas fa-chevron-right"></i>'
+                ]
+            });
+        });
     </script>
 </body>
 
--- resources/views/site/pages/product_details.blade.php
+++ resources/views/site/pages/product_details.blade.php
@@ -1,39 +1,57 @@
 @extends('site.pages.master')
 @section('content')
-    <section id="product_deatils">
-        <div class="container">
+<section id="product_deatils">
+    <div class="container">
         <div class="breadcrumb-section pt-4 py-4">
-                <div class="breadcrumb">
-                    <ul>
-                        <li><a href="{{route('home')}}">Home</a></li>
-                        <li>Product Details</li>
-                    </ul>
+            <div class="breadcrumb">
+                <ul>
+                    <li><a href="{{route('home')}}">Home</a></li>
+                    <li>Product Details</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <div class="product_image_section">
+                    <a href="{{ route('product_details', $product->id) }}"><img src="/product/{{ $product->image }}"
+                            alt="product_deatils_main_img" class="product_deatils_main_img"></a>
+                </div>
+                <div class="row mt-4">
+                    <div class="owl-carousel owl-theme">
+                        <div class="item"><img src="/product/{{ $product->image }}"
+                                alt="product_deatils_main_img" class="product_deatils_main_img"></div>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="product_image_section">
-                        <a href="{{ route('product_details', $product->id) }}"><img src="/product/{{ $product->image }}"
-                            alt="product_deatils_main_img" class="product_deatils_main_img"></a>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $product->name }}</h5>
-                        <p class="card-text" style="text-align: justify;">{{ $product->details }}</p>
-                        @if (is_null($product->discount_price))
-                            <p class="card-text" style="text-align: justify;">Tk. {{ $product->discount_price }}</p>
-                            <p class="card-text" style="text-align: justify;">Tk. <del>{{ $product->price }}</del></p>
-                        @elseif (is_null($product->price))
-                            <p class="card-text" style="text-align: justify;">Tk. {{ $product->price }}</p>
-                        @endif
-                        <form action="{{ route('add.to.cart', $product->id) }}" method="post">
-                            @csrf
-                            <button class="add-btn">ADD TO CART</button>
-                        </form>
-                    </div>
+            <div class="col-md-4">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $product->name }}</h5>
+                    <p class="card-text" style="text-align: justify;">{{ $product->details }}</p>
+                    @if ($product->price > 0)
+                    <p class="card-text">Tk. <del>{{ $product->price }}</del></p>
+                    @endif
+                    <p class="card-text">Tk. {{ $product->discount_price }}</p>
+                    <form action="{{ route('add.to.cart', $product->id) }}" method="post">
+                        @csrf
+                        <button class="add-btn">ADD TO CART</button>
+                    </form>
                 </div>
             </div>
         </div>
-    </section>
+        <div class="related-products">
+            <h3 class="text-center pt-2 pb-2"><strong>Related Products</strong></h3>
+            <div class="owl-carousel owl-theme">
+                <div class="item">
+                    <img src="/product/{{ $product->image }}" alt="product_deatils_main_img" class="product_deatils_main_img">
+                    <h5 class="card-title">{{ $product->name }}</h5>
+                    <p class="card-text">{{ $product->details }}</p>
+                    @if ($product->price > 0)
+                    <p class="card-text">Tk. <del>{{ $product->price }}</del></p>
+                    @endif
+                    <p class="card-text">Tk. {{ $product->discount_price }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
