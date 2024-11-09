b4ee6e41347b4fd6f39826de8838982ace08dbc6
unknown <ahsansagor449@gmail.com>
2024-11-10 00:44:39 +0600

    dashboard design update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,170 +2,95 @@
 @section('page_title', 'Dashboard')
 @section('dashboard_select', 'active')
 @section('container')
-    {{-- @include('admin.partials.header') --}}
-    {{-- <div class="row w-100">
-    <div class="col-sm-12 col-md-3">
-        <div class="item_card">
-            <p>87 <br> Customers</p>
-            <i class="fas fa-users"></i>
-        </div>
+@include('admin.partials.header')
+<div class="dashboard-content">
+    <div class="item_card">
+        <p>87 <br> Customers</p>
+        <i class="fas fa-users"></i>
     </div>
-    <div class="col-sm-12 col-md-3">
-        <div class="item_card">
-            <p>{{ $products }} <br> Products</p>
-            <i class="fas fa-tshirt"></i>
-        </div>
+    <div class="item_card">
+        <p>{{ $products }} <br> Products</p>
+        <i class="fas fa-tshirt"></i>
     </div>
-    <div class="col-sm-12 col-md-3">
-        <div class="item_card">
-            <p>{{ $orders }} <br> Orders</p>
-            <i class="fas fa-cart-arrow-down"></i>
-        </div>
+    <div class="item_card">
+        <p>{{ $orders }} <br> Orders</p>
+        <i class="fas fa-cart-arrow-down"></i>
     </div>
-    <div class="col-sm-12 col-md-3">
-        <div class="item_card">
-            <p>{{ $categories }} <br> Categories</p>
-            <i class="fas fa-tag"></i>
-        </div>
+    <div class="item_card">
+        <p>{{ $categories }} <br> Categories</p>
+        <i class="fas fa-tag"></i>
     </div>
-</div> --}}
-    {{-- <div class="row w-100">
-    <div class="col-md-2">
-        @include('admin.partials.sidebar')
-    </div>
-    <div class="col-md-10 col-sm-12">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="dashboard-content" style="padding: 30px 0">
-                <div class="row">
-                    <div class="col-sm-12 col-md-3">
-                        <div class="item_card">
-                            <p>87 <br> Customers</p>
-                            <i class="fas fa-users"></i>
-                        </div>
-                    </div>
-                    <div class="col-sm-12 col-md-3">
-                        <div class="item_card">
-                            <p>{{ $products }} <br> Products</p>
-                            <i class="fas fa-tshirt"></i>
-                        </div>
-                    </div>
-                    <div class="col-sm-12 col-md-3">
-                        <div class="item_card">
-                            <p>{{ $orders }} <br> Orders</p>
-                            <i class="fas fa-cart-arrow-down"></i>
-                        </div>
-                    </div>
-                    <div class="col-sm-12 col-md-3">
-                        <div class="item_card">
-                            <p>{{ $categories }} <br> Categories</p>
-                            <i class="fas fa-tag"></i>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-8">
-                <div class="dashbaord_card">
-                    <table class="table">
-                        <div style="display: flex;flex-direction:row;justify-content:space-between">
-                            <h4>Top Selling</h4>
-                            <a href="" class="view-btn">View All</a>
-                        </div>
-                        <thead>
-                            <tr>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Handle</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                            <tr>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="dashbaord_card">
-                    <table class="table">
-                        <div style="display: flex;flex-direction:row;justify-content:space-between">
-                            <h4>Uers</h4>
-                            <a href="" class="view-btn">View All</a>
-                        </div>
-                        <thead>
-                            <tr>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Handle</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                            <tr>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> --}}
-    @include('admin.partials.header')
-    <div class="dashboard-content">
-        <div class="item_card">
-            <p>87 <br> Customers</p>
-            <i class="fas fa-users"></i>
-        </div>
-        <div class="item_card">
-            <p>{{ $products }} <br> Products</p>
-            <i class="fas fa-tshirt"></i>
-        </div>
-        <div class="item_card">
-            <p>{{ $orders }} <br> Orders</p>
-            <i class="fas fa-cart-arrow-down"></i>
-        </div>
-        <div class="item_card">
-            <p>{{ $categories }} <br> Categories</p>
-            <i class="fas fa-tag"></i>
-        </div>
-    </div>
+</div>
+<div class="dashbaord_table_area">
+    <table class="table dashbaord_table">
+        <!-- <div class="dashbaord_table_header">
+            <h4>Top Selling</h4>
+            <a href="" class="view-btn">View All</a>
+        </div> -->
+        <thead>
+            <tr>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+            <tr>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
+    <table class="table dashbaord_table">
+        <!-- <div class="dashbaord_table_header">
+            <h4>Top Selling</h4>
+            <a href="" class="view-btn">View All</a>
+        </div> -->
+        <thead>
+            <tr>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+            <tr>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 @endsection
