3321edd710d1b9d405b70f1b5a5a861bfc1a8cb5
unknown <ahsansagor449@gmail.com>
2024-11-19 23:16:51 +0600

    admin dashboard design update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -34,9 +34,9 @@
                         <tr>
                             <th class="text-center">Date</th>
                             <th class="text-center">Customer Name</th>
-                            <th class="text-center">Address</th>
+                            <!-- <th class="text-center">Address</th> -->
                             <th class="text-center">Grand Total</th>
-                            <th class="text-center">Status</th>
+                            <!-- <th class="text-center">Status</th> -->
                         </tr>
                     </thead>
                     <tbody>
@@ -44,16 +44,16 @@
                         <tr>
                             <td>{{ date('d-m-Y', strtotime($order->created_at)) }}</td>
                             <td>{{ $order->name }}</td>
-                            <td>{{ $order->address }}</td>
+                            <!-- <td>{{ $order->address }}</td> -->
                             <td class="text-center">{{ $order->grand_total }}</td>
-                            <td>
+                            <!-- <td>
                                 @if ($order->status == 'pending')
                                 <button class="btn btn-warning"
                                     style="padding: 5px 10px 5px 10px !important; margin: 0 5px !important;">{{ $order->status }}</button>
                                 @else
                                 <button class="btn btn-danger">{{ $order->status }}</button>
                                 @endif
-                            </td>
+                            </td> -->
                         </tr>
                         @endforeach
                     </tbody>
