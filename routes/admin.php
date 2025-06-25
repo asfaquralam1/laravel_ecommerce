14ed1d51ac061907dec543e68d3796d691930ec1
asfaquralam1 <ahsansagor449@gmail.com>
2025-06-25 17:58:35 +0600

    authentication

--- routes/admin.php
+++ routes/admin.php
@@ -5,37 +5,37 @@ use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('admin', [AdminController::class, 'index'])->name('admin.login');
-Route::post('/authenticate', [AdminController::class, 'authenticate'])->name('admin.authenticate');
-Route::get('/logout', [AdminController::class, 'logout'])->name('admin.logout');
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('login');
+    Route::post('/authenticate', [AdminController::class, 'authenticate'])->name('authenticate');
+    Route::get('/logout', [AdminController::class, 'logout'])->name('logout');
 
-// Route::group(['middleware' => ['auth:admin']], function () {
-//     Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-// });
+    Route::middleware('auth:admin')->group(function () {
+        // Route::get('/dashboard', function () {
+        //     return view('admin.dashboard');
+        // })->name('dashboard');
 
-// Route::prefix('admin')->middleware('guest:admin')->group(function () {
-//     Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-// });
+        Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
 
+        // category
+        Route::get('/categories', [CategoryController::class, 'index'])->name('category');
+        Route::get('/manage-category', [CategoryController::class, 'create'])->name('manage.category');
+        Route::post('/add-category', [CategoryController::class, 'store'])->name('add.category');
+        Route::get('/edit-category/{id}', [CategoryController::class, 'edit'])->name('edit.category');
+        Route::post('/update-category/{id}', [CategoryController::class, 'update'])->name('update.category');
+        Route::delete('/destory-category/{id}', [CategoryController::class, 'delete'])->name('destory.category');
+        Route::post('/category/status/{status}/{id}', [CategoryController::class, 'status'])->name('status.category');
 
-Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-
-Route::get('admin/categories', [CategoryController::class, 'index'])->name('admin.category');
-Route::get('admin/manage-category', [CategoryController::class, 'create'])->name('admin.manage.category');
-Route::post('admin/add-category', [CategoryController::class, 'store'])->name('admin.add.category');
-Route::get('admin/edit-category/{id}', [CategoryController::class, 'edit'])->name('admin.edit.category');
-Route::post('admin/update-category/{id}', [CategoryController::class, 'update'])->name('admin.update.category');
-Route::delete('admin/destory-category/{id}', [CategoryController::class, 'delete'])->name('admin.destory.category');
-Route::post('admin/category/status/{status}/{id}', [CategoryController::class, 'status'])->name('admin.status.category');
-//product
-Route::get('admin/products', [ProductController::class, 'product'])->name('admin.product');
-Route::get('admin/manage-product', [ProductController::class, 'manage_product'])->name('admin.manage.product');
-Route::post('admin/add-product', [ProductController::class, 'add_product'])->name('admin.add.product');
-Route::get('admin/edit-product/{id}', [ProductController::class, 'edit_product'])->name('admin.edit.product');
-Route::post('admin/update-product/{id}', [ProductController::class, 'update_product'])->name('admin.update.product');
-Route::get('admin/printbarcode/product', [ProductController::class, 'printbarcode'])->name('admin.printbarcode.product');
-Route::get('admin/product/printbarcode/pdf/{id}', [ProductController::class, 'generateBarcodePdf'])->name('admin.product.barcode.pdf');
-Route::delete('admin/destory-product/{id}', [ProductController::class, 'delete_product'])->name('admin.destory.product');
-//order
-Route::get('admin/order', [AdminController::class, 'order'])->name('admin.order');
-// });
+        //product
+        Route::get('/products', [ProductController::class, 'product'])->name('product');
+        Route::get('/manage-product', [ProductController::class, 'manage_product'])->name('manage.product');
+        Route::post('/add-product', [ProductController::class, 'add_product'])->name('add.product');
+        Route::get('/edit-product/{id}', [ProductController::class, 'edit_product'])->name('edit.product');
+        Route::post('/update-product/{id}', [ProductController::class, 'update_product'])->name('update.product');
+        Route::get('/printbarcode/product', [ProductController::class, 'printbarcode'])->name('printbarcode.product');
+        Route::get('/product/printbarcode/pdf/{id}', [ProductController::class, 'generateBarcodePdf'])->name('product.barcode.pdf');
+        Route::delete('/destory-product/{id}', [ProductController::class, 'delete_product'])->name('destory.product');
+        //order
+        Route::get('/orders', [AdminController::class, 'order'])->name('order');
+    });
+});
